1c4d2fc93acbdb026b3991b04c10fc8763220796
mihneabranzeu <mihneacnitv@gmail.com>
2025-01-20 19:22:13 +0200

    Initial commit for project setup

---
+++ eventplanning/src/test/java/com/example/eventplanning/EventplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventplanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
